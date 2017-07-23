750b34f5aa2eda52846b964cb7122997c8baebf1
Pradeep Banavara <pradeepbs@gmail.com>
2017-07-23 07:37:23 +0530

    Improvements over last commit
    Deleted unwanted future looking files
    Collectionview displays dates, current date highlighted
    Collectionview highlight tapped date

---
+++ cal/cal/CalendarCellCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarCellCollectionViewCell.swift
+//  cal
+//
+//  Created by pbanavara on 7/22/17.
+//  Copyright © 2017 pbanavara. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var day_label: UILabel!
+}
--- cal/cal/ViewController.swift
+++ cal/cal/ViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,
-                        UICollectionViewDelegate, UICollectionViewDataSource {
-
-    
+                        UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    @IBOutlet weak var navBar: UINavigationBar!
     @IBOutlet weak var calendarView: UICollectionView!
-    
-    
     @IBOutlet weak var agendaView: UITableView!
+    
+    //Constants
+    let calendarViewCellColor = UIColor.blue
+    
     var dates = [Int]()
+    var noOfDays = 7
+    var highlightIndex = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,8 +35,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         agendaView.dataSource = self
         agendaView.delegate = self
         
+        navBar.titleTextAttributes = ["June": "June"]
+        let dayOfMonth = getDate()
+        //highlightIndex = dates.index(of: dayOfMonth)!
+        highlightIndex = 23
+        print(highlightIndex)
+    }
+    
+    func getDate() -> Int {
+        
+        let date = Date()
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day], from: date)
+        
+        let year =  components.year
+        let month = components.month
+        let day = components.day
+        return day!
     }
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -51,14 +77,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = calendarView.dequeueReusableCell(withReuseIdentifier: "reuseCalendarCell", for: indexPath)
-        cell.backgroundColor = UIColor.blue
+        let cell = calendarView.dequeueReusableCell(withReuseIdentifier: "reuseCalendarCell", for: indexPath) as! CalendarCellCollectionViewCell
+        if(indexPath.row == highlightIndex) {
+            cell.day_label.backgroundColor = calendarViewCellColor
+        }
+        cell.day_label.text = String(dates[indexPath.row])
         return cell
         
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
+        let cell = calendarView.cellForItem(at: indexPath) as! CalendarCellCollectionViewCell
+        cell.day_label.backgroundColor = calendarViewCellColor
+        let element = dates.remove(at: indexPath.row)
+        print(dates)
     }
     
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
@@ -69,6 +101,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = agendaView.dequeueReusableCell(withIdentifier: "agendaItem")!
         return cell
         
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        let totalSpace = flowLayout.sectionInset.left
+            + flowLayout.sectionInset.right
+            + (flowLayout.minimumInteritemSpacing * CGFloat(noOfDays - 1))
+        let size = Int((collectionView.bounds.width - totalSpace) / CGFloat(noOfDays))
+        return CGSize(width: size, height: size)
     }
     
 
